ba71588c55573ab5b559bc7b62f7af47485b8ce2
Jaski-waterloo <55409139+Jaski-waterloo@users.noreply.github.com>
2019-10-24 14:33:49 -0400

    Update PageRankNode.java

--- src/main/java/io/bespin/java/mapreduce/pagerank/PageRankNode.java
+++ src/main/java/io/bespin/java/mapreduce/pagerank/PageRankNode.java
@@ -51,22 +51,26 @@ public class PageRankNode implements Writable {
 
   private Type type;
   private int nodeid;
-  private Float pagerank;
+  private float pagerank;
+  private ArrayListOfIntsWritable adjacenyList;
   private ArrayListOfFloatsWritable pageranks;
-  private ArrayListOfIntsWritable adjacencyList;
 
   public PageRankNode() {}
 
-  public ArrayListOfFloatsWritable getPageRank() {
+  public float getPageRank() {
     return pagerank;
   }
-  
-  public void setPageRank(Float p){
+
+  public ArrayListOfFloatsWritable getPageRanks() {
+    return pageranks;
+  }
+
+  public void setPageRank(float p) {
     this.pagerank = p;
   }
 
   public void setPageRanks(ArrayListOfFloatsWritable p) {
-    this.pagerank = p;
+    this.pageranks = p;
   }
 
   public int getNodeId() {
@@ -77,12 +81,12 @@ public class PageRankNode implements Writable {
     this.nodeid = n;
   }
 
-  public ArrayListOfIntsWritable getAdjacencyList() {
-    return adjacencyList;
+  public ArrayListOfIntsWritable getAdjacenyList() {
+    return adjacenyList;
   }
 
   public void setAdjacencyList(ArrayListOfIntsWritable list) {
-    this.adjacencyList = list;
+    this.adjacenyList = list;
   }
 
   public Type getType() {
@@ -97,7 +101,6 @@ public class PageRankNode implements Writable {
    * Deserializes this object.
    *
    * @param in source for raw byte representation
-   * @throws IOException if any exception is encountered during object deserialization
    */
   @Override
   public void readFields(DataInput in) throws IOException {
@@ -118,15 +121,14 @@ public class PageRankNode implements Writable {
       pageranks.readFields(in);
     }
 
-    adjacencyList = new ArrayListOfIntsWritable();
-    adjacencyList.readFields(in);
+    adjacenyList = new ArrayListOfIntsWritable();
+    adjacenyList.readFields(in);
   }
 
   /**
    * Serializes this object.
    *
    * @param out where to write the raw byte representation
-   * @throws IOException if any exception is encountered during object serialization
    */
   @Override
   public void write(DataOutput out) throws IOException {
@@ -135,27 +137,30 @@ public class PageRankNode implements Writable {
 
     if (type.equals(Type.Mass)) {
       out.writeFloat(pagerank);
+      pageranks.write(out);
       return;
     }
 
     if (type.equals(Type.Complete)) {
       out.writeFloat(pagerank);
+      pageranks.write(out);
     }
 
-    adjacencyList.write(out);
+    adjacenyList.write(out);
   }
 
   @Override
   public String toString() {
-    return String.format("{%d %.4f %s}", nodeid, pagerank, (adjacencyList == null ? "[]"
-        : adjacencyList.toString(10)));
+    return String.format("{%d %.4f %s %s}", nodeid, pagerank, 
+        (pageranks == null ? "[]" : pageranks.toString(10)),
+        (adjacenyList == null ? "[]" : adjacenyList.toString(10)));
   }
 
   /**
    * Returns the serialized representation of this object as a byte array.
    *
    * @return byte array representing the serialized representation of this object
-   * @throws IOException if any exception is encountered during object serialization
+   * @throws IOException
    */
   public byte[] serialize() throws IOException {
     ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
@@ -170,7 +175,7 @@ public class PageRankNode implements Writable {
    *
    * @param in source for reading the serialized representation
    * @return newly-created object
-   * @throws IOException if any exception is encountered during object deserialization
+   * @throws IOException
    */
   public static PageRankNode create(DataInput in) throws IOException {
     PageRankNode m = new PageRankNode();
@@ -184,7 +189,7 @@ public class PageRankNode implements Writable {
    *
    * @param bytes raw serialized representation
    * @return newly-created object
-   * @throws IOException if any exception is encountered during object deserialization
+   * @throws IOException
    */
   public static PageRankNode create(byte[] bytes) throws IOException {
     return create(new DataInputStream(new ByteArrayInputStream(bytes)));
